9277c0c205fff61ea3d7d78e1ed11b4b88a07288
Scottishprog <m_wanamaker_jr@sbcglobal.net>
2024-05-21 15:37:15 -0700

    Completed second word -> intitial ray trace code.

---
+++ src/ray.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def unit_vector(vector):
+    norm = np.linalg.norm(vector)
+    if norm == 0:
+        return vector
+    return vector / norm
+
+
+class Ray:
+    def __init__(self, origin, direction): # Both inputs are size 3 vectors.
+        self.origin = origin
+        self.direction = direction
+
+    def at(self, t):
+        return self.origin + t * self.direction
